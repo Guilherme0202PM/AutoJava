34426c7a39086beeb480dff68074fd79059637cd
Guilherme <guilhermeperes0202@gmail.com>
2026-04-17 13:35:04 -0300

    Aula 3 - Herança

---
+++ src/Java/Funcoes/Professor.java
@@ -0,0 +1,34 @@
+package Java.Funcoes;
+
+public class Professor extends Pessoa {
+
+    private double salario;
+    private String disciplina;
+
+    public Professor() {
+        super(); // chama o construtor vazio de Pessoa
+    }
+
+    public Professor(String nome, int idade, double altura, double salario, String disciplina) {
+        super(nome, idade, altura); // chama o construtor de Pessoa
+        this.salario = salario;
+        this.disciplina = disciplina;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    // Getter e Setter para disciplina
+    public String getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(String disciplina) {
+        this.disciplina = disciplina;
+    }
+}
